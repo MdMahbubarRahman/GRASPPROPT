3d4a3d625330822fd36594cc30c105e093eeb804
Md Mahbubar Rahman <mrah06@gmail.com>
2021-10-06 06:04:29 -0500

    ga framework

--- Geneticalgorithm.cpp
+++ Geneticalgorithm.cpp
@@ -2,6 +2,103 @@
 
 
 
+//default constructor
+Chromosome::Chromosome() {
+	fitness    = 0.0;
+	separatorInt  = 0;
+	isFeasible    = false;
+	reProductionProbability   = 0.0;
+	reInsertionProbabililty   = 0.0;
+	mutationProbability       = 0.0;
+	size = 0;
+}
+
+//copy constructor
+Chromosome::Chromosome(const Chromosome& chrom) {
+	chromosome = chrom.chromosome;
+	fitness = chrom.fitness;
+	separatorInt = chrom.separatorInt;
+	isFeasible = chrom.isFeasible;
+	reProductionProbability = chrom.reProductionProbability;
+	reInsertionProbabililty = chrom.reInsertionProbabililty;
+	mutationProbability = chrom.mutationProbability;
+	size = chrom.size;
+}
+
+//returns chromosome
+std::list<int> Chromosome::getChromosome() {
+
+}
+
+//returns fitness value
+double Chromosome::getFitness() {
+
+}
+
+//prints fitness value
+void Chromosome::showFitness() {
+
+}
+
+//updates to feasible chromosome
+void Chromosome::updateToFeasibleChromosome() {
+
+}
+
+//returns seperator integer value
+int Chromosome::getSeparatorInt() {
+
+}
+
+//prints separator integer
+void Chromosome::showSeparatorInt() {
+
+}
+
+//returns feasibility status
+bool Chromosome::getFeasibilityStatus() {
+
+}
+
+//checks whether the chromosome is feasible
+void Chromosome::checkFeasiblity() {
+
+}
+
+//returns reproduction probability
+double Chromosome::getReproductionProbability() {
+
+}
+
+//returns reinsertion probability
+double Chromosome::getReinsertionProbability() {
+
+}
+
+//returns mutation probability
+double Chromosome::getMutationProbability() {
+
+}
+
+//updates reproduction and reinsertion probabilities
+void Chromosome::updateReproductionAndReinsertionProbabilities() {
+
+}
+
+//updates mutation probability
+void Chromosome::updateMutationProbability() {
+
+}
+
+//returns size of the chromosome
+int Chromosome::getSize() {
+
+}
+
+//updates size of the chromosome
+void Chromosome::updateSize(int size) {
+
+}
 
 //default constructor
 Elitism::Elitism() {
@@ -33,6 +130,40 @@ CrossOver::CrossOver(const CrossOver & crossr) {
 
 }
 
+//performs partially mapped crossover operation
+void CrossOver::performPertiallyMappedCrossover() {
+
+}
+
+//prints parent 1
+void CrossOver::showParent1() {
+
+}
+
+//prints parent 2
+void CrossOver::showParent2() {
+
+}
+
+//prints offspring
+void CrossOver::showOffspring() {
+
+}
+
+//returns parent 1
+Chromosome CrossOver::getParent1() {
+
+}
+
+//returns parent 2
+Chromosome CrossOver::getParent2() {
+
+}
+
+//returns offspring
+Chromosome CrossOver::getOffspring() {
+
+}
 
 //default constructor
 Mutation::Mutation() {
@@ -44,13 +175,39 @@ Mutation::Mutation(const Mutation & mutatn){
 
 }
 
+//returns premutation chromosome
+Chromosome Mutation::getOriginalChromosome() {
+
+}
+
+//returns mutated chromosome
+Chromosome Mutation::getMutatedChromosome() {
+
+}
+
+//prints premutation chromosome
+void Mutation::showOriginalChromosome() {
+
+}
+
+//prints mutated chromosome
+void Mutation::showMutatedChromosome() {
+
+}
+
+//performs chain mutation
+void Mutation::performMutation() {
+
+}
+
+
 //default constructor
-Generation::Generation() {
+Population::Population() {
 
 }
 
 //copy constructor
-Generation::Generation(const Generation & gentn) {
+Population::Population(const Population & gentn) {
 
 }
 
@@ -64,6 +221,10 @@ Tournament::Tournament(const Tournament & tourmnt){
 
 }
 
+//arrange tournament
+void Tournament::arrangeTournament() {
+
+}
 
 //default constructor
 Geneticalgorithm::Geneticalgorithm() {
@@ -75,8 +236,50 @@ Geneticalgorithm::Geneticalgorithm(const Geneticalgorithm & ga) {
 
 }
 
+//selects elit chromosome from current generation
+void Geneticalgorithm::selectElitChromosome() {
 
+}
 
+//arranges tournament with current generation population
+void Geneticalgorithm::arrangeTournament() {
+
+}
+
+//performs partially mapped crossover
+void Geneticalgorithm::performPartiallyMappedCrossover() {
+
+}
+
+//performs mutation operation
+void Geneticalgorithm::performChainMutation() {
+
+}
+
+//prints current generation population
+void Geneticalgorithm::showCurrentPopulation() {
+
+}
+
+//prints future generation population
+void Geneticalgorithm::showFuturePopulation() {
+
+}
+
+//returns current generation population
+Population Geneticalgorithm::getCurrentPopulation() {
+	return currentPopulation;
+}
+
+//returns future generation population
+Population Geneticalgorithm::getFuturePopulation() {
+	return futurePopulation;
+}
+
+//runs genetic algorithm 
+void Geneticalgorithm::runGeneticAlgorithm() {
+
+}
 
 
 
--- Geneticalgorithm.h
+++ Geneticalgorithm.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <list>
 
 #ifndef GENETICALGOIRTHM_H
 #define GENETICALGOIRTHM_H
@@ -22,10 +24,48 @@ More knowledge to develop above functions and algorithms can be found from follo
 	3. Active guided evolution strategies for large-scale vehicle routing problems with time windows by D Mester, O Bräysy 
 */
 
+/*
+The initial populatioon is made up of an individual obtained from the Clarke and Wright heuristic (1964) and a number of individuals with 
+chromosomes randomly generated. During the whole evolutionary process the population size remains constant.
+*/
+
+//chromosome
+//chromosomes are fixed size
+class Chromosome {
+private:
+	std::list<int> chromosome;
+	double fitness;
+	int separatorInt;
+	bool isFeasible;
+	double reProductionProbability;
+	double reInsertionProbabililty;
+	double mutationProbability;
+	int size;//size of the chromosome 
+public:
+	Chromosome();//default constructor
+	Chromosome(const Chromosome & chrom);//copy constructor
+	std::list<int> getChromosome();
+	double getFitness();
+	void showFitness();
+	int getSize();
+	void updateSize(int size);
+	void updateToFeasibleChromosome();
+	int getSeparatorInt();
+	void showSeparatorInt();
+	bool getFeasibilityStatus();
+	void checkFeasiblity();
+	double getReproductionProbability();
+	double getReinsertionProbability();
+	double getMutationProbability();
+	void updateMutationProbability();
+	void updateReproductionAndReinsertionProbabilities();
+};
+
+
 
 //class for elitism operator
 class Elitism {
-
+	Chromosome elit;
 public:
 	Elitism();//default constructor
 	Elitism(const Elitism & elit);//copy constructor
@@ -33,7 +73,7 @@ public:
 
 //class for replication operator
 class Replication {
-
+	Chromosome replica;
 public:
 	Replication();//default constructor
 	Replication(const Replication & replica);//copy constructor
@@ -41,43 +81,78 @@ public:
 
 //class for cross over operator
 class CrossOver {
-
+private:
+	Chromosome parent1;
+	Chromosome parent2;
+	Chromosome offspring;
 public:
 	CrossOver();//default constructor
 	CrossOver(const CrossOver & crossr);//copy constructor
+	void performPertiallyMappedCrossover();
+	void showParent1();
+	void showParent2();
+	void showOffspring();
+	Chromosome getParent1();
+	Chromosome getParent2();
+	Chromosome getOffspring();
 };
 
 //class for mutation operator
 class Mutation {
-
+	Chromosome originalChrom;
+	Chromosome mutatedChrom;
 public:
 	Mutation();//default constructor
 	Mutation(const Mutation & mutatn);//copy constructor
-
+	Chromosome getOriginalChromosome();
+	Chromosome getMutatedChromosome();
+	void showOriginalChromosome();
+	void showMutatedChromosome();
+	void performMutation();
 };
 
-//class for a gereration of population
-class Generation {
-
+//class for a of population
+class Population {
+	std::list<std::list<int>> population;
 public:
-	Generation();//default constructor
-	Generation(const Generation & gentn);//copy constructor
+	Population();//default constructor
+	Population(const Population & poplatn);//copy constructor
+	std::list<std::list<int>> getPopulation();
+	void showPopulation();
 };
 
 //class for tournament
 class Tournament {
-
+private:
+	Population population;
 public:
 	Tournament();//default constructor
 	Tournament(const Tournament & tourmnt);//copy constructor
+	void arrangeTournament();
 };
 
 //class for genetic algorithm
 class Geneticalgorithm{
-
+private:
+	Population currentPopulation;
+	Population futurePopulation;
+	Elitism elitism;
+	Replication replication;
+	Mutation mutation;
+	CrossOver crossover;
+	Tournament tournament;
 public:
 	Geneticalgorithm();//default constructor
 	Geneticalgorithm(const Geneticalgorithm & ga);//copy constructor	
+	void selectElitChromosome();
+	void arrangeTournament();
+	void performPartiallyMappedCrossover();
+	void performChainMutation();
+	void showCurrentPopulation();
+	void showFuturePopulation();
+	Population getCurrentPopulation();
+	Population getFuturePopulation();
+	void runGeneticAlgorithm();
 };
 
 
