28d79fb3a29a2c4f41db299e03904e3cb23e5b73
Md Mahbubar Rahman <mrah06@gmail.com>
2021-10-21 18:36:34 -0500

    refactored

--- Localsearch.h
+++ Localsearch.h
@@ -6,6 +6,7 @@
 #include <queue>
 
 #include "Geneticalgorithm.h"
+#include "Initialsolution.h"
 
 
 #ifndef LOCALSEARCH_H
@@ -20,64 +21,6 @@ More insight of this algorithm can be found from the following paper:
 	1. Clustering-based heuristics for the two-echelon capacitated vehicle routing problem by
 	   TG Crainic, S Mancini, G Perboli, R Tadei
 */
-
-//class cvrp solution 
-class CVRPSolution {
-private:
-	std::vector<int> solution;
-	std::set<int> customers;
-	std::map<int, int> customerTodemand;
-	double solCost = 0;
-	int satelliteNode = 0;
-	int maxRouteCapacity = 0;
-	int totalDemandSatisfied = 0;
-	int numberOfRoutes = 0;
-public:
-	CVRPSolution();//default constructor
-	CVRPSolution(std::vector<int> sol, std::set<int> customers, std::map<int, int> customerTodemand, double cost, int satelliteNode, int maxRouteCapacity, int totalDemandSatisfied, int numberOfRoutes);//constructor
-	CVRPSolution(const CVRPSolution & sol); //copy constructor
-	void showSolution() const;
-	void showCustomers();
-	void showCustomerTodemandMap();
-	void showSolCost();
-	void showSatelliteNode();
-	void showMaxRouteCapacity();
-	void showTotalDemandSatisfied();
-	void showNumberOfRoutes();
-	std::vector<int> getSolution();
-	int getSatelliteNode();
-	double getCost();
-	int getMaxRouteCapacity();
-	std::set<int> getCustomers();
-	std::map<int, int> getCustomerTodemandMap();
-	int getTotalDemandSatisfied();
-	int getNumberOfRoutes();
-};
-
-//local solution class
-class TwoEchelonSolution {
-private:
-	int numberOfActiveSatellites = 0;
-	std::map<int, int> customerToDemandMap;
-	std::map<int, int> satelliteToDemandMap;
-	std::vector<std::vector<double>> distanceMatrix;
-	std::set<int> customerNodes;
-	std::set<int> satelliteNodes;
-	CVRPSolution firstEchelonSolution;
-	std::list<CVRPSolution> secondEchelonSolutions;
-public:
-	TwoEchelonSolution();
-	TwoEchelonSolution(const TwoEchelonSolution & locSol);
-	TwoEchelonSolution(int numberOfActiveSatellites, std::map<int, int> customerToDemandMap, std::map<int, int> satelliteToDemandMap, std::vector<std::vector<double>> distanceMatrix, std::set<int> customerNodes,	std::set<int> satelliteNodes, CVRPSolution firstEchelonSol, std::list<CVRPSolution> secondEchelonSols);
-	int getNumberOfActiveSatellites();
-	std::map<int, int> getCustomerToDemandMap();
-	std::map<int, int> getSatelliteToDemandMap();
-	std::vector<std::vector<double>> getDistanceMatrix();
-	std::set<int> getCustomerNodes();
-	std::set<int> getSatelliteNodes();
-	CVRPSolution getFirstEchelonSolution();
-	std::list<CVRPSolution> getSecondEchelonSolutions();
-};
 
 //customer to depots potential assignment class
 class CustomerDepotDifferentialCost {
