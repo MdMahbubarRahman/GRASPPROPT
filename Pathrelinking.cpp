7338205730f20c85db64830a48cc9b020f6116ff
Md Mahbubar Rahman <mrah06@gmail.com>
2021-11-03 05:06:28 -0500

    pathrelinking framework

--- Pathrelinking.cpp
+++ Pathrelinking.cpp
@@ -1,3 +1,123 @@
 #include "Pathrelinking.h"
 
 //Implement Path Relinking algoirthm here
+
+//default constructor
+CustomerStatus::CustomerStatus() {
+	cusID = 0;
+	satIDInBestSol = 0;
+	satIDInLocalSol = 0;
+	distanceDiff = 0;
+}
+
+//copy constructor
+CustomerStatus::CustomerStatus(const CustomerStatus& cusStat) {
+	cusID = cusStat.cusID;
+	satIDInBestSol = cusStat.satIDInBestSol;
+	satIDInLocalSol = cusStat.satIDInLocalSol;
+	distanceDiff = cusStat.distanceDiff;
+}
+
+//constructor
+CustomerStatus::CustomerStatus(int cID, int satIDLocalSol, int satIDBestSol, double distDiff) {
+	cusID = cID;
+	satIDInBestSol = satIDBestSol;
+	satIDInLocalSol = satIDLocalSol;
+	distanceDiff = distDiff;
+}
+
+//returns customer id
+int CustomerStatus::getCusID() {
+	return cusID;
+}
+
+//returns satellite id for the local solution
+int CustomerStatus::getLocalSatID() {
+	return satIDInLocalSol;
+}
+
+//returns satellite id for the best solution
+int CustomerStatus::getBestSatID() {
+	return satIDInBestSol;
+}
+
+//returns relative distance difference
+double CustomerStatus::getDistanceDiff() {
+	return distanceDiff;
+}
+
+//prints customer's status
+void CustomerStatus::showCustomerStatus() {
+	std::cout << "\nShow customer ID : " << cusID << std::endl;
+	std::cout << "Satellite ID in the local solution : " << satIDInLocalSol << std::endl;
+	std::cout << "Satellite ID in the best solution : " << satIDInBestSol << std::endl;
+	std::cout << "Relative distance difference : " << distanceDiff << std::endl;
+}
+
+//comparator
+bool SortTool::operator()(CustomerStatus& a, CustomerStatus& b) {
+	return (a.getDistanceDiff() > b.getDistanceDiff());
+}
+
+//default constructor
+Pathrelinking::Pathrelinking() {
+	std::cout << "The default constructor of the path relinking class has been called!" << std::endl;
+}
+
+//copy constructor
+Pathrelinking::Pathrelinking(const Pathrelinking& pathRelkn) {
+	localSolution = pathRelkn.localSolution;
+	bestSolution = pathRelkn.bestSolution;
+	intermediateSolution = pathRelkn.intermediateSolution;
+	customersToBeReassigned = pathRelkn.customersToBeReassigned;
+}
+
+//constructor
+Pathrelinking::Pathrelinking(TwoEchelonSolution localSol, TwoEchelonSolution bestSol) {
+	localSolution = localSol;
+	bestSolution = bestSol;
+}
+
+//returns local solution
+TwoEchelonSolution Pathrelinking::getLocalSolution() {
+	return localSolution;
+}
+
+//returns best solution
+TwoEchelonSolution Pathrelinking::getInitialBestSolution() {
+	return bestSolution;
+}
+
+//returns intermediate solution
+TwoEchelonSolution Pathrelinking::getIntermediateSolution() {
+	return intermediateSolution;
+}
+
+//returns best solution
+TwoEchelonSolution Pathrelinking::getCurrentBestSolution() {
+	runPathRelinking();
+	return bestSolution;
+}
+
+//prints local solution
+void Pathrelinking::showLocalSolution() {
+	localSolution.showTwoEchelonSolution();
+}
+
+//prints best solution
+void Pathrelinking::showBestSolution() {
+	bestSolution.showTwoEchelonSolution();
+}
+
+//prints intermediate solution
+void Pathrelinking::showIntermediateSolution() {
+	intermediateSolution.showTwoEchelonSolution();
+}
+
+//runs the path relinking algorithm
+void Pathrelinking::runPathRelinking() {
+	//needs implementation
+}
+
+
+
--- Pathrelinking.h
+++ Pathrelinking.h
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <queue>
+#include <list>
+#include <vector>
+
+#include "Initialsolution.h"
 
 #ifndef PATHRELINKING_H
 #define PATHRELINKING_H 
@@ -10,13 +15,53 @@ TODO: Develop Path Relinking algoirthm based on following papers:
 	2. The two-echelon vehicle routing problem by S Mancini 
 */
 
-class Pathrelinking{
+//customer status class
+class CustomerStatus {
+private:
+	int cusID;
+	int satIDInLocalSol;
+	int satIDInBestSol;
+	double distanceDiff;
+public:
+	CustomerStatus();
+	CustomerStatus(const CustomerStatus & cusStat);
+	CustomerStatus(int cusID, int satIDInLocalSol, int satIDInBestSol, double distanceDiff);
+	int getCusID();
+	int getLocalSatID();
+	int getBestSatID();
+	double getDistanceDiff();
+	void showCustomerStatus();
+};
 
+//comparator
+class SortTool {
+public:
+	bool operator()(CustomerStatus &a , CustomerStatus &b);
+};
 
+//path relinking class
+class Pathrelinking {
+private:
+	TwoEchelonSolution localSolution;
+	TwoEchelonSolution bestSolution;
+	TwoEchelonSolution intermediateSolution;
+	std::priority_queue<CustomerStatus, std::vector<CustomerStatus>, SortTool> customersToBeReassigned;
+public:
+	Pathrelinking();
+	Pathrelinking(const Pathrelinking & pathRelkn);
+	Pathrelinking(TwoEchelonSolution localSol, TwoEchelonSolution bestSol);
+	TwoEchelonSolution getLocalSolution();
+	TwoEchelonSolution getInitialBestSolution();
+	TwoEchelonSolution getIntermediateSolution();
+	TwoEchelonSolution getCurrentBestSolution();
+	void showLocalSolution();
+	void showBestSolution();
+	void showIntermediateSolution();
+	void runPathRelinking();
 };
 
 
-
-
 #endif // !PATHRELINKING_H
+
+
 
